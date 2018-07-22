327e74d260321baf2c11cfa7d245f3d975374001
hongliang777 <yhlhsc7@163.com>
2018-07-22 12:39:37 +0800

    first commit

---
+++ src/main/java/com/yhl/log4j2/LookupFromApp.java
@@ -0,0 +1,25 @@
+package com.yhl.log4j2;
+
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.lookup.StrLookup;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongliang
+ * Date: 18-7-22
+ * Time: 上午11:28
+ * To change this template use File | Settings | File Templates.
+ */
+@Plugin(name = "lk", category = StrLookup.CATEGORY)
+public class LookupFromApp implements StrLookup{
+    @Override
+    public String lookup(String key) {
+        return "f:/yhl/logs1/";
+    }
+
+    @Override
+    public String lookup(LogEvent event, String key) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ src/main/java/com/yhl/log4j2/PropertySubstitutionTest.java
@@ -0,0 +1,21 @@
+package com.yhl.log4j2;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: hongliang
+ * Date: 18-7-22
+ * Time: 上午10:21
+ * To change this template use File | Settings | File Templates.
+ */
+public class PropertySubstitutionTest {
+
+    public static Logger logger = LogManager.getLogger(PropertySubstitutionTest.class);
+
+    public static void main(String[] args) {
+        logger.info("test{},{}","abc","bcd");
+    }
+}
